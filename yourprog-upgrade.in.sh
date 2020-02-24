54daf7208476091b0c6671c86873484eb4d6b9f8
kristapsdz <kristaps@bsd.lv>
2020-02-24 23:21:51 +0000

    Sync with slant's way of doing things.

--- yourprog-upgrade.in.sh
+++ yourprog-upgrade.in.sh
@@ -1,26 +1,53 @@
 #! /bin/sh
 
-set -e
+KWBP="@SHAREDIR@/yourprog/yourprog.kwbp"
+
+args=`getopt f: $*`
+if [ $? -ne 0 ]
+then
+	echo 'Usage: ...'
+	exit 2
+fi
+set -- $args
+while [ $# -ne 0 ]
+do
+	case "$1" in
+	-f)
+		KWBP="$2"; shift; shift;;
+	--)
+		shift; break;;
+        esac
+done
 
 if [ ! -f "@DATADIR@/yourprog.db" ]
 then
+	# If the database doesn't exist, obviously nothing's running.
+	# Simply install it and exit.
+	set -e
 	mkdir -p "@DATADIR@"
 	echo "@DATADIR@/yourprog.db: installing new"
-	kwebapp-sql "@SHAREDIR@/yourprog/yourprog.kwbp" | sqlite3 "@DATADIR@/yourprog.db"
+	ort-sql "$KWBP" | sqlite3 "@DATADIR@/yourprog.db"
 	chown www "@DATADIR@/yourprog.db"
 	chmod 600 "@DATADIR@/yourprog.db"
-	install -m 0444  "@SHAREDIR@/yourprog/yourprog.kwbp" "@DATADIR@/yourprog.kwbp"
-	chmod 555 "@CGIBIN@/yourprog"
+	install -m 0444 "$KWBP" "@DATADIR@/yourprog.kwbp"
 	exit 0
 fi
 
+cmp -s "$KWBP" "@DATADIR@/yourprog.kwbp"
+if [ $? -eq 0 ]
+then
+	echo "@DATADIR@/yourprog.db: already up to date"
+	exit 0
+fi
+
+set -e
 TMPFILE=`mktemp` || exit 1
 trap "rm -f $TMPFILE" ERR EXIT
 
 echo "@DATADIR@/yourprog.db: patching existing"
 
 ( echo "BEGIN EXCLUSIVE TRANSACTION;" ; \
-  kwebapp-sqldiff "@DATADIR@/yourprog.kwbp"  "@SHAREDIR@/yourprog/yourprog.kwbp" ; \
+  ort-sqldiff "@DATADIR@/yourprog.kwbp"  "$KWBP" ; \
   echo "COMMIT TRANSACTION;" ; ) > $TMPFILE
 
 if [ $? -ne 0 ]
@@ -30,7 +57,6 @@ then
 fi
 
 sqlite3 "@DATADIR@/yourprog.db" < $TMPFILE
-install -m 0444  "@SHAREDIR@/yourprog/yourprog.kwbp" "@DATADIR@/yourprog.kwbp"
-chmod 555 "@CGIBIN@/yourprog"
+install -m 0444 "$KWBP" "@DATADIR@/yourprog.kwbp"
 rm -f "@DATADIR@/yourprog-upgrade.sql"
 echo "@DATADIR@/yourprog.db: patch success"
